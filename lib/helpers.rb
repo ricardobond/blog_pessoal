89044174212cefa1bad7e490880f4db639ab6eaf
Ricardo Caldeira <ricardo.nezz@gmail.com>
2012-08-18 17:47:27 -0300

    Implementação do botão like facebook

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -4,21 +4,17 @@ def copia_static
   FileUtils.cp_r 'static/.', 'output/static/'
 end
 
-# Gera arquivos meta para css baseados no filtro
-def css_meta(filtro = "less")
+# Gera arquivos meta para css baseados na extensão do filtro (ex.: sass, less...)
+def load_style_links(filtro = "less")
   nome_do_arquivo = ""
-  meta_css = []
   meta_css_final = ""
   @items.each do |i|
     if i[:extension] == filtro
-      nome_do_arquivo = i[:filename].sub("content/less/", "").sub(filtro, "css") if i[:extension] == filtro
-      if nome_do_arquivo[0] != "_"
-        meta_css << nome_do_arquivo
+      nome_do_arquivo = i[:filename].sub("content/less/", "").sub(filtro, "css")
+      if !nome_do_arquivo.start_with?("_")
+        meta_css_final << "<link rel='stylesheet' href='#{@config[:css_dir]}#{nome_do_arquivo}' media='screen' rel='stylesheet' type='text/css' />\n"
       end
     end
-  end
-  meta_css.each do |meta|
-    meta_css_final << "<link rel='stylesheet' href='#{@config[:css_dir]}#{meta}' media='screen' rel='stylesheet' type='text/css' />\n"
   end
   return meta_css_final
 end
